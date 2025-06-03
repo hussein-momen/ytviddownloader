ddea4b748e5ab11adec98c7dbc13f8e8bfa7cdbe
hussein-momen <husseinjunior344@gmail.com>
2025-06-04 02:33:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from pytube import YouTube
+print("YouTube Video Downloader v?.? - Beta")
+url = input("Enter Video URL: ").strip()
+yt = get_video(url)
+print(f"\nTitle: {yt.title}")
+print(f"Author: {yt.author}")
+streams
